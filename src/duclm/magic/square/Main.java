2bf7094de253749a4053d45916f584984704f2af
Le Minh Duc <duclmdev@gmail.com>
2018-01-29 22:52:13 +0700

    Update Main.java

--- src/duclm/magic/square/Main.java
+++ src/duclm/magic/square/Main.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int n = 10 + new Random().nextInt(90);
-        n = 16;
+        //n = 16;
         System.out.println("n = " + n);
 
         try {
